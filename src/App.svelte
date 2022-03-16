004eb2b253aa4fcddbc40d609acc430858cac21f
santhitak <santhita.sk@gmail.com>
2022-03-16 13:56:27 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import TailwindCss from "./TailwindCSS.svelte";
+  import Magic8 from "./lib/Magic8.svelte";
+</script>
+
+<main>
+  <TailwindCss />
+  <Magic8 />
+</main>
+
+<style>
+  :root {
+    font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
+      "Liberation Mono", "Courier New", monospace;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/lib/Magic8.svelte
@@ -0,0 +1,23 @@
+<div class="ball-wrapper flex justify-center align-center">
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Magic_eight_ball.png"
+    alt="Magic ball"
+    style="height: 75vh; width: auto;"
+  />
+  <div class="absolute">
+    <h5 class="prediction font-mono">Probably</h5>
+  </div>
+</div>
+
+<style>
+  .ball-wrapper {
+    height: 100%;
+    width: 100%;
+    align-items: center;
+  }
+  .prediction {
+    color: #7ee1ff;
+    font-weight: 600;
+    font-size: large;
+  }
+</style>
