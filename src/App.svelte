0fcb97194d3402f1f78b4d3e8ba12f9d9ce06570
santhitak <santhita.sk@gmail.com>
2022-05-30 13:57:53 +0700

    refactor: move css to emotion js

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import TailwindCss from "./TailwindCSS.svelte";
   import { prediction } from "./assets/prediction";
+  import { main, bn632, bn20, wrapper, ballWrapper, predict_txt, intro } from './assets/styles.js'
 
   let predict = "Ask and shake";
   function pred(e){
@@ -14,23 +15,25 @@
 </script>
 
 <main>
-  <TailwindCss />
-  <div class="wrapper">
-    <div class="intro">
-      <h2>MAGIC <br /> 8 BALL</h2>
-      <p>Do you ever feel hesitated?</p>
-      <p>Let's the magic 8 ball tell you what to do!</p>
-      <button class="bn632-hover bn20" on:click={pred} type="button"> SHAKE </button>
-    </div>
-    <div class="ball-wrapper flex justify-center align-center">
-      <img
-        src="https://i.imgur.com/oO3tsTC.png"
-        alt="Magic ball"
-        id="magicBall"
-        style="height: 75vh; width: auto; padding-right: 10rem"
-      />
-      <div class="absolute">
-        <h5 class="prediction font-mono" id="pred">{predict}</h5>
+  <div class="{main}">
+    <TailwindCss />
+    <div class="{wrapper}">
+      <div class="{intro} intro">
+        <h2>MAGIC <br /> 8 BALL</h2>
+        <p>Do you ever feel hesitated?</p>
+        <p>Let's the magic 8 ball tell you what to do!</p>
+        <button class="{bn632} bn632 {bn20}" on:click={pred} type="button"> SHAKE </button>
+      </div>
+      <div class="{ballWrapper} flex justify-center align-center">
+        <img
+                src="https://i.imgur.com/oO3tsTC.png"
+                alt="Magic ball"
+                id="magicBall"
+                style="height: 75vh; width: auto; padding-right: 10rem"
+        />
+        <div class="absolute">
+          <h5 class="{predict_txt} font-mono" id="pred">{predict}</h5>
+        </div>
       </div>
     </div>
   </div>
---
+++ src/assets/styles.js
@@ -0,0 +1,77 @@
+import { css } from '@emotion/css'
+
+export const main = css`
+  height: 100vh;
+  width: 100vw;
+  background: rgb(185, 250, 255);
+  background: radial-gradient(
+          circle,
+          rgba(185, 250, 255, 1) 0%,
+          rgba(35, 62, 236, 1) 40%,
+          rgba(0, 0, 0, 1) 80%
+  );
+  overflow: hidden;
+  padding: 0 4rem;
+`
+
+export const bn632 = css`
+  width: 160px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #fff;
+  cursor: pointer;
+  margin: 20px;
+  height: 55px;
+  text-align: center;
+  border: none;
+  background-size: 300% 100%;
+  border-radius: 50px;
+  -o-transition: all 0.4s ease-in-out;
+  -webkit-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
+`
+
+export const bn20 = css`
+  background-image: linear-gradient(
+          to right,
+          #66c5ea,
+          #2041d3,
+          #66c5ea,
+          #2041d3
+  );
+  margin-left: 0;
+  margin-top: 3rem;
+  box-shadow: 0 4px 15px 0 rgba(116, 79, 168, 0.75);
+`
+
+export const wrapper = css`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const predict_txt = css`
+  color: #7ee1ff;
+  max-width: 7vw;
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: medium;
+  margin-right: 9.55rem;
+  margin-bottom: 3rem;
+`
+
+export const ballWrapper = css`
+  height: 100%;
+  width: 100%;
+  align-items: center;
+  user-select: none;
+`
+
+export const intro = css`
+  width: 40vw;
+  height: 100vh;
+  padding: 10rem;
+`
